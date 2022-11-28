e4de289839996ea0ce9ef4191955d247ef50f979
yanooomm <115118650+yanooomm@users.noreply.github.com>
2022-11-28 19:13:02 +0500

    Add files via upload

---
+++ дз7-1.cpp
@@ -0,0 +1,27 @@
+﻿// str21 Дана строка s переведите строку в нижний регистр
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+char to_lowercase(char c)
+{
+    if (c >= 'A' && c <= 'Z') 
+    {
+        return c + 32;
+    }
+    return c;
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.length(); i++) 
+    {
+        char c = to_lowercase(s[i]);
+        cout << c;
+    }
+
+}
